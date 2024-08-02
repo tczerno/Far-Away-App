df20b7a8ab48f5f407e4bc16fdbd2eca28585d15
Todd Czerno <todd.czerno@outlook.com>
2024-08-02 14:28:45 -0400

    building app components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
-export default function App(){
-  
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LISST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
